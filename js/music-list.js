fe59b439a78cbcc90d854a8bb92824ad3676b248
mochharun <mochharun93@gmail.com>
2024-05-17 00:16:16 +0700

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,92 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Parody Of The Music",
+    artist: "Kamlun",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Watch Me",
+    artist: "Silentó",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "STAY",
+    artist: "The Kid LAROI,Justin Bieber",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Hotline Bling",
+    artist: "Drake",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Terminator",
+    artist: "King Promise feat. Young Jonn",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Calm Down ",
+    artist: "Rema feat Selena Gomez",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Love Yourself",
+    artist: "Justin Bieber",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Here's Your Perfect",
+    artist: "Jamie Miller",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Sacrifice",
+    artist: "Elton John",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "I Started A Joke",
+    artist: "The Beegees",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "How Can I Tell Her",
+    artist: "Lobo",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "I'd Love You to Want Me",
+    artist: "Lobo",
+    img: "music-11",
+    src: "music-11"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
